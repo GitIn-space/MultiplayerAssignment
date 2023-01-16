914736f73aa1e59ed493af30f72f37cc931e1cf5
David Bång <dadda121@gmail.com>
2023-01-16 15:08:35 +0100

    testing join event

--- Assets/Scripts/UI/GameHUD.cs
+++ Assets/Scripts/UI/GameHUD.cs
@@ -1,16 +1,21 @@
+using Alteruna;
 using UnityEngine;
-
 public class GameHUD : MonoBehaviour
 {
     [SerializeField] private GameObject _playerList;
     [SerializeField] private GameObject _playerUIPrefab;
-
+    [SerializeField] private Multiplayer _mp;
 
     [ContextMenu("Add Player")]
-    void AddPlayerToList()
+    void AddPlayerToList(Multiplayer mp, User user)
     {
-        Instantiate(_playerUIPrefab, _playerList.transform);
+        PlayerUI ui = Instantiate(_playerUIPrefab, _playerList.transform).GetComponent<PlayerUI>();
+        ui.SetName(user.Name);
     }
 
+    private void Awake()
+    {
+        _mp.OtherUserJoined.AddListener(AddPlayerToList);
+    }
 
 }
